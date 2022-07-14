7c59cb212b48b1a778d69cc5f95e3eb206eb74fd
bea <anna.eu.bia@gmail.com>
2022-07-14 10:56:52 -0300

    Inicializacao do destaques

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Destaques from './Destaques/Destaques';
 import Menu from './Menu/Menu';
 
 function App() {
   return (
     <div className="App">
       <Menu />
+      <Destaques />
     </div>
   );
 }
